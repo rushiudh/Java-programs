a4d08697d790e3a34ad5427b84bf7c0fc2935c46
Rushikesh Udhoji <32307213+rushiudhoji@users.noreply.github.com>
2020-10-12 15:34:24 +0530

    Array Duplicate

--- arrayop.java
+++ arrayop.java
@@ -1,38 +1,31 @@
-import java.util.Scanner;
-class arrayop
-{
-  int ChangeArray(int a[],int n)
-  {
-    if(n==0 || n==1)
-    {
-      return n;
+import java.util.Arrays;
+public class arrayop{
+public static int removeDuplicateElements(int arr[], int n){
+        if (n==0 || n==1){
+            return n;
+        }
+        int[] temp = new int[n];
+        int j = 0;
+        for (int i=0; i<n-1; i++){
+            if (arr[i] != arr[i+1]){
+                temp[j++] = arr[i];
+            }
+         }
+        temp[j++] = arr[n-1];
+        // Changing original array
+        for (int i=0; i<j; i++){
+            arr[i] = temp[i];
+        }
+        return j;
     }
-    int temp[] = new int[n];
-    int j=0;
-    for(int i=0;i<n-1;i++)
-    {
-      if(arr[i]!=arr[i+1])
-        temp[j++] = arr[i];
-    }
-    temp[j++] = arr[n-1];
 
-    for(int i =0;i<j;i++)
-    {
-      arr[i] = temp[i];
+    public static void main (String[] args) {
+        int arr[] = {10,70,10,30,90,20,20,30,20,70};//unsorted array
+        Arrays.sort(arr);//sorting array
+        int length = arr.length;
+        length = removeDuplicateElements(arr, length);
+        //printing array elements
+        for (int i=0; i<length; i++)
+           System.out.print(arr[i]+" ");
     }
-    return j;
-  }
-public static void main(String[]arg)
-{
-  int intarray[];
-  intarray = new int[10];
-  Scanner Sc = new Scanner(System.in);
-  for(int i=0; i<intarray.length;i++)
-    intarray[i] = Sc.nextInt();
-    int length = ChangeArray(intarray,intarray.length);
-    for(int i=0;i<length;i++)
-    {
-        System.out.println(intarray[i]+" ");
-    }
-  }
 }
